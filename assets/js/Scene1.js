e02f206447665f6a5ba924d0528165dd3f884d4c
Aditya Fitriadi <39262811+featriadi@users.noreply.github.com>
2023-03-28 13:42:59 +0700

    sprite

---
+++ assets/js/Scene1.js
@@ -0,0 +1,34 @@
+class Scene1 extends Phaser.Scene {
+    constructor() {
+        super("bootGame");
+    }
+
+    preload() {
+        this.load.image("background", "assets/images/background.png");
+
+        this.load.spritesheet("ship1", "assets/images/spritesheets/ship.png", {
+            frameWidth: 16,
+            frameHeight: 16,
+        });
+
+        this.load.spritesheet("ship2", "assets/images/spritesheets/ship2.png", {
+            frameWidth: 32,
+            frameHeight: 16,
+        });
+
+        this.load.spritesheet("ship3", "assets/images/spritesheets/ship3.png", {
+            frameWidth: 32,
+            frameHeight: 32,
+        });
+
+        this.load.spritesheet("explosion", "assets/images/spritesheets/explosion.png", {
+            frameWidth: 16,
+            frameHeight: 16,
+        });
+    }
+
+    create() {
+        this.add.text(20, 20, "Loading game...");
+        this.scene.start("playGame");
+    }
+}
